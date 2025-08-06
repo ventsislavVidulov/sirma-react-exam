a9bd047ebc8561909aa10ffc0b170801d92def82
ventsislavVidulov <xcen3k@gmail.com>
2025-08-06 16:13:47 +0300

    fix typo of constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const MOVIES_FILE_PATH = '/data/movies.csv';
 export const ACTORS_FILE_PATH = '/data/actors.csv';
-export const ROLES_FILEPATH = '/data/roles.csv';
+export const ROLES_FILE_PATH = '/data/roles.csv';
