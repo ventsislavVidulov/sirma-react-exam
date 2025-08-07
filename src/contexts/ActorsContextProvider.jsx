444c7b98c419972a77d9a1ead999382c9740929c
ventsislavVidulov <xcen3k@gmail.com>
2025-08-07 20:40:34 +0300

    create actors context provider

---
+++ src/contexts/ActorsContextProvider.jsx
@@ -0,0 +1,54 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import actorsService from "../services/actorsService";
+import { simulatedDelay } from "../utils/simulatedDelay";
+import { DELAY_IN_MILISECONDS } from "../constants";
+
+const ActorsContext = createContext({});
+
+const ActorsContextProvider = ({ children }) => {
+    const [actors, setActors] = useState([]);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchActors = async () => {
+            try {
+                await simulatedDelay(DELAY_IN_MILISECONDS); //custom delay simulation for loading ui testing
+                setActors(await actorsService.getAll());
+            } catch (error) {
+                setError(error.message);
+                console.error(error.message);
+            } finally {
+                setLoading(false);
+            } 
+        };
+        fetchActors();
+    }, []);
+
+    useEffect(() => {
+        if (!error && !loading) {
+            console.log('Actors context loaded');
+        }
+    }, [actors, error, loading]);
+
+    const contextObject = {
+        actors,
+        error,
+        loading,
+        getActorById: actorsService.getById,
+        getTopActors: actorsService.getTopActors
+    };
+
+    return (
+        <ActorsContext.Provider value={contextObject}>
+            {children}
+        </ActorsContext.Provider>
+    )
+};
+
+export const useActors = () => {
+    const context = useContext(ActorsContext);
+    return context;
+};
+
+export { ActorsContextProvider };
