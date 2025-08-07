665dfb0e494773f94adbde45a3fffcd7ca2e8f8d
ventsislavVidulov <xcen3k@gmail.com>
2025-08-07 20:41:21 +0300

    wrap actors context provider aroun app

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
+import { ActorsContextProvider } from './contexts/ActorsContextProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+    <ActorsContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ActorsContextProvider >
+  </StrictMode>
 );
