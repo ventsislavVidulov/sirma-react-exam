bb5b4d90610afe30838e6978802d76f06370f746
ventsislavVidulov <xcen3k@gmail.com>
2025-08-07 20:42:28 +0300

    add content to Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,36 @@
-import { ActorCard } from "../../components";
 import styles from "./Home.module.css";
+import { ActorCard } from "../../components";
+import { useActors } from "../../contexts/ActorsContextProvider";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+    const [topActors, setTopActors] = useState([]);
+    const [actors, setActors] = useState([]);
+    const actorsContext = useActors();
+
+    useEffect(() => {
+        const fetchTopActors = async () => {
+            try {
+                setTopActors(await actorsContext.getTopActors());
+                // console.log(await actorsContext.actors);
+                setActors(await actorsContext.actors)
+            } catch (error) {
+                console.error(error.message);
+            }
+        }
+        fetchTopActors();
+    }, [])
+ 
     return (
         <>
             <h1>Home</h1>
-            <div className={styles.container}>
-                <ActorCard />
-                <ActorCard />
-            </div>
+            {actorsContext.loading ?
+                <h1>Loading</h1> :
+                <div className={styles.container}>
+                    <ActorCard />
+                    <ActorCard />
+                </div>
+            }
         </>
     )
 };
