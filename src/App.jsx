49b0147d2f4a94687a1c218dd02cf787b3dbf141
ventsislavVidulov <xcen3k@gmail.com>
2025-08-07 13:13:11 +0300

    add BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import moviesService from './services/moviesService';
 import rolesService from './services/rolesService';
 import actorsService from './services/actorsService';
 import { getTheActorsThatActedToghetherInMostMovies } from './utils/getTheActorsThatActedToghetherInmostMovies';
+import { Navigation } from "./components"
+import { Route, Routes } from 'react-router';
+import { Home } from './pages';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -22,26 +25,10 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navigation></Navigation>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
