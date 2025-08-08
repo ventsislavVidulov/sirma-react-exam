288f561a13b460bbb2547acc988f42a01a77c2d4
ventsislavVidulov <xcen3k@gmail.com>
2025-08-08 20:26:51 +0300

    add actors route

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import './App.css';
 import { Navigation } from "./components";
 import { Route, Routes } from 'react-router';
 import { Actors, Home, Movies } from './pages';
+import { ActorDetails } from "./components";
 
 function App() {
 
   return (
     <>
-      <Navigation/>
+      <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/movies' element={<Movies/>}/>
-        <Route path='/actors' element={<Actors/>}/>
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/actors' element={<Actors />} />
+        <Route path='/actors/:actorId' element={<ActorDetails />} />
       </Routes>
     </>
   )
