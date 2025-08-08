964af2d373a23477600468d2fa0e15d292605032
ventsislavVidulov <xcen3k@gmail.com>
2025-08-08 10:59:02 +0300

    refactor to export on method declaration

--- src/services/actorsService.js
+++ src/services/actorsService.js
@@ -5,44 +5,46 @@ import { getTheActorsThatActedToghetherInMostMovies } from "../utils/getTheActor
 
 let actors = [];
 
-const getAll = async () => {
-    if (actors.length === 0) {
+export default {
+    async getAll() {
+        if (actors.length === 0) {
+            try {
+                actors = await mapCSVToObject(CSVReaderAsync, ACTORS_FILE_PATH);
+            } catch (error) {
+                console.error(error.message);
+                throw new Error(error.message);
+            }
+        }
+        return [...actors]; //returns new reference to avoid mutation
+    },
+
+    async getById(actorId) { //returns new reference of the object we are looking for or 'No actor found with this id'
         try {
-            actors = await mapCSVToObject(CSVReaderAsync, ACTORS_FILE_PATH);
+            const actor = (await this.getAll()).find(a => a.ID === actorId);
+            if (actor) {
+                return { ...actor }; //returns new reference to avoid mutations
+            } else {
+                throw new Error('No actor found with this id')
+            }
         } catch (error) {
             console.error(error.message);
             throw new Error(error.message);
         }
-    }
-    return [...actors]; //returns new reference to avoid mutation
-};
+    },
 
-const getById = async (actorId) => { //returns new reference of the object we are looking for or 'No actor found with this id'
-    try {
-        const actor = (await getAll()).find(a => a.ID === actorId);
-        if (actor) {
-            return { ...actor }; //returns new reference to avoid mutations
-        } else {
-            throw new Error('Can\'t find actor with this id')
+    async getTopActors()  {
+        try {
+            const topActors = await getTheActorsThatActedToghetherInMostMovies();
+            return topActors;
+        } catch (error) {
+            console.error(error.message)
+            throw new Error(error.message);
         }
-    } catch (error) {
-        console.error(error.message);
-        throw new Error(error.message);
     }
-};
+}
 
-const getTopActors = async () => {
-    try {
-        const topActors = await getTheActorsThatActedToghetherInMostMovies();
-        return topActors;
-    } catch (error) {
-        console.error(error.message)
-        throw new Error(error.message);
-    }
-};
-
-export default {
-    getAll,
-    getById,
-    getTopActors
-};
+// export default {
+//     getAll,
+//     getById,
+//     getTopActors
+// };
