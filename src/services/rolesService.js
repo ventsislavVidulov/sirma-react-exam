5f4fe7e9559d74417b8b94a72f87c54315e9e81e
ventsislavVidulov <xcen3k@gmail.com>
2025-08-08 10:59:18 +0300

    add error handling

--- src/services/rolesService.js
+++ src/services/rolesService.js
@@ -2,20 +2,30 @@ import { ROLES_FILE_PATH } from "../constants";
 import { mapCSVToObject } from "../utils/objectCSVMapper";
 import { CSVReaderAsync } from "../utils/CSVParser";
 
+let roles = [];
+
 export default {
     async getAll() {
-        const roles = await mapCSVToObject(CSVReaderAsync, ROLES_FILE_PATH);
+        try {
+            roles = await mapCSVToObject(CSVReaderAsync, ROLES_FILE_PATH);
+        } catch (error) {
+            console.error(error.message);
+            throw new Error(error.message);
+        }
         return [...roles]; //returns new reference to avoid mutation
     },
 
     async getById(roleId) { //returns new reference of the object we are looking for or 'No role found with this id'
-        const role = (await this.getAll()).find(r => r.ID = roleId);
-
-        if (role) {
-            return { ...role }; //returns new reference to avoid mutations
-        } else {
-            console.log('No role found with this id');
-            return 'No role found with this id';
+        try {
+            const role = (await this.getAll()).find(r => r.ID = roleId);
+            if (role) {
+                return { ...role }; //returns new reference to avoid mutations
+            } else {
+                throw new Error('No role found with this id');
+            }
+        } catch (error) {
+            console.error(error.message);
+            throw new Error(error.message);
         }
-    }
+    },
 }
