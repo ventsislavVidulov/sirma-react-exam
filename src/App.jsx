a79fcb4e52913152d994b233c50d90370e2ff51e
ventsislavVidulov <xcen3k@gmail.com>
2025-08-08 16:42:02 +0300

    remove the unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import moviesService from './services/moviesService';
-import rolesService from './services/rolesService';
-import actorsService from './services/actorsService';
-import { getTheActorsThatActedToghetherInMostMovies } from './utils/getTheActorsThatActedToghetherInmostMovies';
 import { Navigation } from "./components";
 import { Route, Routes } from 'react-router';
 import { Actors, Home, Movies } from './pages';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    const loadMovies = async () => {
-      await getTheActorsThatActedToghetherInMostMovies();
-      const allMovies = await moviesService.getAll();
-      setMovies(allMovies)
-    }
-    loadMovies();
-  }, []);
 
   return (
     <>
