a93d490c560c49d34d1606c89382d08da51650d6
ventsislavVidulov <xcen3k@gmail.com>
2025-08-08 16:41:43 +0300

    remove the unused contexts

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App.jsx';
-import { ActorsContextProvider } from './contexts/ActorsContextProvider.jsx';
-import { RolesContextProvider } from './contexts/RolesContextProvider.jsx';
+import { DataContextProvider } from './contexts/DataContextProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RolesContextProvider>
-      <ActorsContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ActorsContextProvider >
-    </RolesContextProvider>
+        <DataContextProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </DataContextProvider >
   </StrictMode>
 );
