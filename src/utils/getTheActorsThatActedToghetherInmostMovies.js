5ba6364c48512095ce9d4c6fa0d9696011d4c28c
ventsislavVidulov <xcen3k@gmail.com>
2025-08-08 11:00:11 +0300

    refactor to hold the top actors ids as  array instead of string

--- src/utils/getTheActorsThatActedToghetherInmostMovies.js
+++ src/utils/getTheActorsThatActedToghetherInmostMovies.js
@@ -11,30 +11,31 @@ export const getTheActorsThatActedToghetherInMostMovies = async () => {
         curnetMovie.hasOwnProperty('actors') ? curnetMovie.actors.push(r.ActorID) : curnetMovie.actors = [r.ActorID];
     });
 
-    const actorPairs = []; //array for all unique actor pairs of type {pairIds: '1,2', commonMoviesCount: 2, commonMovies: [3, 15]}
+    const actorPairs = []; //array for all unique actor pairs of type {pairIds: [1,2], commonMoviesCount: 2, commonMovies: [3, 15]}
     movies.forEach(m => {
         if (m.actors) {
             const currentActorsIdsArray = [...m.actors] //convert ids to numbers and sort them to avoid duplicating of "type" '2,4' and '4,2'
-            .map(aId => Number(aId))
-            .sort((a, b) => a - b);
+                .map(aId => Number(aId))
+                .sort((a, b) => a - b);
             for (let i = 0; i < currentActorsIdsArray.length - 1; i++) {
                 for (let j = i + 1; j < currentActorsIdsArray.length; j++) {
                     const firstActorId = currentActorsIdsArray[i];
                     const secondActorId = currentActorsIdsArray[j];
-                    const currentActorPair = actorPairs.find(ap => ap.pairIds === `${firstActorId},${secondActorId}`);
+                    const currentActorPair = actorPairs.find(ap => ap.pairIds[0] === firstActorId && ap.pairIds[1] === secondActorId);
                     if (currentActorPair) {
                         currentActorPair.commonMoviesCount++;
                         currentActorPair.commonMovies.push(m.ID);
                     } else {
-                        actorPairs.push({ pairIds: `${firstActorId},${secondActorId}`, commonMoviesCount: 1, commonMovies: [m.ID] });
+                        actorPairs.push({ pairIds: [firstActorId, secondActorId], commonMoviesCount: 1, commonMovies: [m.ID] });
                     }
                 }
             }
         }
     });
     actorPairs.sort((a, b) => b.commonMoviesCount - a.commonMoviesCount); //sort the actor pairs common movies count in descending order
-    const topActorsPairMoviesCount = Number(actorPairs[0].commonMoviesCount); 
+    const topActorsPairMoviesCount = Number(actorPairs[0].commonMoviesCount);
     const topActorPairs = []; //returning array of top actors pairs if they are more than one
     actorPairs.forEach(ap => ap.commonMoviesCount === topActorsPairMoviesCount ? topActorPairs.push(ap) : null);
+    
     return topActorPairs;
 };
