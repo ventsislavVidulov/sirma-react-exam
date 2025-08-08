10ff3dd1123e4892589720f9bafd92da53d72f0c
ventsislavVidulov <xcen3k@gmail.com>
2025-08-08 12:58:52 +0300

    add error handling

--- src/services/moviesService.js
+++ src/services/moviesService.js
@@ -2,20 +2,34 @@ import { MOVIES_FILE_PATH } from "../constants";
 import { mapCSVToObject } from "../utils/objectCSVMapper";
 import { CSVReaderAsync } from "../utils/CSVParser";
 
+let movies = [];
+
 export default {
     async getAll() {
-        const movies = await mapCSVToObject(CSVReaderAsync, MOVIES_FILE_PATH);
+        if (movies.length === 0) {
+            try {
+                movies = await mapCSVToObject(CSVReaderAsync, MOVIES_FILE_PATH);
+            } catch (error) {
+                console.error(error.messge);
+                throw new Error(error.message)
+            }
+            console.log(movies)
+        }
         return [...movies]; //returns new reference to avoid mutation
     },
 
     async getById(movieId) { //returns new reference of the object we are looking for or 'No movie found with this id'
-        const movie = (await this.getAll()).find(m => m.ID = movieId);
-
-        if (movie) {
-            return {...movie}; //returns new reference to avoid mutations
-        } else {
-            console.log('No movie found with this id');
-            return 'No movie found with this id';
+        try {
+            const movie = (await this.getAll()).find(m => m.ID = movieId);
+            if (movie) {
+                return { ...movie }; //returns new reference to avoid mutations
+            } else {
+                throw new Error("No movie found with this id");
+            }
+        } catch (error) {
+            console.error(error.message);
+            throw new Error(error.message);
         }
+
     }
 }
