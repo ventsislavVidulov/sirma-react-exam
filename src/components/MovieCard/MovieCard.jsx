99e257b40402fec5f74c636171f58569866eba2a
ventsislavVidulov <xcen3k@gmail.com>
2025-08-09 10:56:17 +0300

    add props

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 import styles from "./MovieCard.module.css";
 
-const MovieCard = () => {
+const MovieCard = ({movieId, movieName}) => {
  return (
         <Link to={`/movies/${movieId}`} className={styles.container}>
             <h1>{movieName}</h1>
-            <img src="/film-movie-reel-icon" alt="Movie poster" className={styles.image}/>
+            <img src="/film-movie-reel-icon.svg" alt="Movie poster" className={styles.image}/>
         </Link>
     )
 }
