ee394d00f774f07b8e2ea991d5c00aacca5b84c7
ventsislavVidulov <xcen3k@gmail.com>
2025-08-09 10:57:28 +0300

    add cards visualization

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
 import styles from "./Movies.module.css";
+import { MovieCard } from "../../components";
+import { useMovies } from "../../contexts/MoviesContextProvider";
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const moviesContext = useMovies();
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        setMovies(await moviesContext.movies);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    fetchMovies();
+  }, [moviesContext.loading, moviesContext.error]);
+
   return (
-    <h1>Movies</h1>
+    <>
+      <h1>Movies</h1>
+      {moviesContext.loading
+        ? <h1>Loading...</h1>
+        : moviesContext.error
+          ? <h1>{moviesContext.error}</h1>
+          : <div className={styles.container}>
+            {movies.map(m => <MovieCard movieId={m.ID} movieName={m.Title} />)}
+          </div>
+      }
+    </>
   )
 };
 
