b6250d8b059e4562bcc961721565cae5a72f7127
ventsislavVidulov <xcen3k@gmail.com>
2025-08-10 20:20:58 +0300

    add toggle on links

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from "react";
+
 import styles from "./Navigation.module.css";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 const Navigation = () => {
+  const location = useLocation();
+  const [active, setActive] = useState('');
+
+  useEffect(() => {
+    setActive(location.pathname);
+  }, [location.pathname]);
+
   return (
     <nav>
-      <Link className={[styles.link]} to={"/"}>Home</Link>
-      <Link className={styles.link} to={"/movies"}>Movies</Link>
-      <Link className={styles.link} to={"/actors"}>Actors</Link>
+      <Link
+        className={active === "/"
+          ? `${styles.link} ${styles.linkActive}`
+          : styles.link
+        }
+        to={"/"}>Home
+      </Link>
+      <Link
+        className={active === "/movies"
+          ? `${styles.link} ${styles.linkActive}`
+          : styles.link
+        }
+        to={"/movies"}>
+        Movies
+      </Link>
+      <Link
+        className={active === "/actors"
+          ? `${styles.link} ${styles.linkActive}`
+          : styles.link
+        }
+        to={"/actors"}>
+        Actors
+      </Link>
     </nav>
   )
 };
