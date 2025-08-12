f5a99dca2315bc8ee1ac907cfb3054bb97b6f8bf
ventsislavVidulov <xcen3k@gmail.com>
2025-08-12 18:50:39 +0300

    create movies query keys

---
+++ src/queries/moviesQuery/movieQueryKeys.js
@@ -0,0 +1,9 @@
+export const movieQueryKeys = {
+    all: ['movies'],
+    details: (movieId) => [...movieQueryKeys.all, 'details', movieId],
+    // search: (query) => [...movieQueryKeys.all, 'search', query],
+    update: (movieId) => [...movieQueryKeys.details(movieId), 'update'],
+};
+
+
+
