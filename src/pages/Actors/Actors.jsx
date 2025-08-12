05983823ffd074a814f5f5d44dc2b1d57adb1b7b
ventsislavVidulov <xcen3k@gmail.com>
2025-08-12 18:49:43 +0300

    add error displaying

--- src/pages/Actors/Actors.jsx
+++ src/pages/Actors/Actors.jsx
@@ -3,15 +3,15 @@ import { ActorCard } from "../../components";
 import { useGetActors } from "../../queries/actorsQuery/useGetActors";
 
 const Actors = () => {
-  const { data: actors, isError: error, isFetching: loading } = useGetActors();
+  const { data: actors, isError, error, isFetching: loading } = useGetActors();
 
   return (
     <section className={styles.page}>
       <h1>Actors</h1>
       {loading
         ? <h1>Loading...</h1>
-        : error ?
-          <h1 className={styles.error}>{actorsQuery.error}</h1>
+        : isError ?
+          <h1 className={styles.error}>{error.message}</h1>
           : <div className={styles.container}>
             {actors.map(a => (
               <ActorCard key={Number(a.ID)} actorName={a.FullName} actorId={a.ID} />
