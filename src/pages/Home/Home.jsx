d4a13a0fea3201e99696c390e3d67ee11163a2f7
ventsislavVidulov <xcen3k@gmail.com>
2025-08-13 11:41:04 +0300

    add render logic if more than one top actor pair exist

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,23 +3,25 @@ import { ActorCard } from "../../components";
 import { useGetTopActors } from "../../queries/actorsQuery/useGetTopActors";
 
 const Home = () => {
-    const { isError: error, isFetching: loading, data: topActors,  } = useGetTopActors();
+    const { isError: error, isFetching: loading, data: topActors, } = useGetTopActors();
 
     return (
         <section className={styles.page}>
             <h1>Wellcome to the movie database</h1>
-            <h1>Our top actors are</h1>
-            {loading ?
-                <h1>Loading...</h1> :
-                error ?
-                    <h1 className={styles.error}>{actorsContext.error}</h1> :
-                    topActors.map(ta =>
-                    (< div className={styles.container} key={`${ta.pairIds[0]}${ta.pairIds[1]}}`}>
-                        <ActorCard actorName={ta.pairNames[0]} actorId={ta.pairIds[0]} />
-                        <ActorCard actorName={ta.pairNames[1]} actorId={ta.pairIds[1]} />
-                    </div >)
-                    )
+            {loading
+                ? <h1>Loading...</h1>
+                : error
+                    ? <h1 className={styles.error}>{actorsContext.error}</h1>
+                    : topActors.length === 1
+                        ? <h1>Our top actors are</h1>
+                        : <h1>We have {topActors.length} top actor pairs</h1>
             }
+            {topActors.map(ta =>
+            (< div className={styles.container} key={`${ta.pairIds[0]}${ta.pairIds[1]}}`}>
+                <ActorCard actorName={ta.pairNames[0]} actorId={ta.pairIds[0]} />
+                <ActorCard actorName={ta.pairNames[1]} actorId={ta.pairIds[1]} />
+            </div >)
+            )}
         </section>
     )
 };
