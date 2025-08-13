1aff7cd4ffa85f1fd46b6d55c4cf601034cd6314
ventsislavVidulov <xcen3k@gmail.com>
2025-08-13 13:34:31 +0300

    rename inpt params for more clearnes

--- src/components/DeatilsHeader/ActorDetailsHeader.jsx
+++ src/components/DeatilsHeader/ActorDetailsHeader.jsx
@@ -5,20 +5,20 @@ import { CustomButton, CustomFormFieldTitle, FaSave, FaGear, FaDelete, DateInput
 
 import styles from "./ActorDetailsHeader.module.css";
 
-const ActorDetailsHeader = ({ details: { title: actorName, actorId, info: actorBirthDate } }) => {
+const ActorDetailsHeader = ({ details: { title: actorNameProp, actorId: actorIdProp, info: actorBirthDateProp } }) => {
     const [editing, setEditing] = useState(false);
-    const [title, setTitle] = useState(actorName);
+    const [title, setTitle] = useState(actorNameProp);
     const tempTitle = useRef('');
-    const [date, setDate] = useState(actorBirthDate);
+    const [date, setDate] = useState(actorBirthDateProp);
     const tempDate = useRef('');
-    const updateActorQuery = useUpdateActor(actorId);
+    const updateActorQuery = useUpdateActor(actorIdProp);
 
     useEffect(() => {
         if (editing) {
-            tempTitle.current = (title || actorName);
-            tempDate.current = (date || actorBirthDate);
+            tempTitle.current = (title || actorNameProp);
+            tempDate.current = (date || actorBirthDateProp);
         }
-    }, [editing, title, actorName, date, actorBirthDate]);
+    }, [editing, title, actorNameProp, date, actorBirthDateProp]);
 
     const saveHandler = () => {
         if (tempTitle.current.trim() || title) {
@@ -55,7 +55,7 @@ const ActorDetailsHeader = ({ details: { title: actorName, actorId, info: actorB
                 <div className={styles.headerContainer}>
                     {editing
                         ? <>
-                            <CustomFormFieldTitle label={title || actorName} fieldChangeHandler={fieldChangeHandler}></CustomFormFieldTitle>
+                            <CustomFormFieldTitle label={title || actorNameProp} fieldChangeHandler={fieldChangeHandler}></CustomFormFieldTitle>
                             <div className={styles.buttonsContainer}>
                                 <CustomButton>
                                     <FaDelete />
@@ -66,7 +66,7 @@ const ActorDetailsHeader = ({ details: { title: actorName, actorId, info: actorB
                             </div>
                         </>
                         : <>
-                            <h1>{title || actorName}</h1>
+                            <h1>{title || actorNameProp}</h1>
                             <CustomButton handleClickFunction={() => setEditing(!editing)}>
                                 <FaGear />
                             </CustomButton>
